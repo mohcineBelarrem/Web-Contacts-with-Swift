ebe3fad5e461fb2e5daba3479936e6d7d5082ce3
Mohcine Belarrem <belarrem@idm.fr>
2015-07-23 16:42:20 +0200

    Initial Commit

---
+++ Web Contacts with SwiftTests/Web_Contacts_with_SwiftTests.swift
@@ -0,0 +1,36 @@
+//
+//  Web_Contacts_with_SwiftTests.swift
+//  Web Contacts with SwiftTests
+//
+//  Created by mohcine  on 7/23/15.
+//  Copyright (c) 2015 mohcine . All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Web_Contacts_with_SwiftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
